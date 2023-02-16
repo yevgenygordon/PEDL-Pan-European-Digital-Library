c457636bbc75828c07e51d6749399d6d19a9c544
yevgenygordon <ygordon1978@icloud.com>
2023-02-16 15:10:16 +0100

    Initial Commit

---
+++ PEDL/PEDLApp.swift
@@ -0,0 +1,17 @@
+//
+//  PEDLApp.swift
+//  PEDL
+//
+//  Created by Yevgeny Gordon on 16.02.23.
+//
+
+import SwiftUI
+
+@main
+struct PEDLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
